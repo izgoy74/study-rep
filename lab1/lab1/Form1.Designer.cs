7ac63c7c826606b1823bbf51768aa690c84aafa9
izgoy74 <70099191+izgoy74@users.noreply.github.com>
2023-02-16 13:05:54 +0700

    Add files via upload

---
+++ lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pbox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.data = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.data)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.Controls.Add(this.data);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(603, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(282, 551);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pbox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(603, 551);
+            this.panel2.TabIndex = 1;
+            // 
+            // pbox
+            // 
+            this.pbox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pbox.Location = new System.Drawing.Point(0, 91);
+            this.pbox.Name = "pbox";
+            this.pbox.Size = new System.Drawing.Size(603, 352);
+            this.pbox.TabIndex = 0;
+            this.pbox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(169, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Рисование ломаной линии мышью";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(213, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(294, 449);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Выбрать цвет ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // data
+            // 
+            this.data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data.Location = new System.Drawing.Point(3, 3);
+            this.data.Name = "data";
+            this.data.RowTemplate.Height = 25;
+            this.data.Size = new System.Drawing.Size(276, 167);
+            this.data.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 551);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Рисование ломаной линии по точкам";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.data)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView data;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbox;
+    }
+}
+
---
+++ lab1/lab1/breakline.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace lab1
+{//класс предназначен для хранения точек ломаной линии и отображения ломаной линни на picturebox 
+    public class Breakline
+    {
+        //поля 
+        Point[] points = null;
+        int N = 0;
+        int maxN = 0; //переделать 
+        //конструктор
+        public Breakline(DataGridView data, int n = 50)
+        {
+            maxN = n; //выделяем память для массива
+            points = new Point[maxN];
+            //настраиваем таблицу для вывода координат точек
+            data.ColumnCount = 2;
+            data.RowCount = 0;
+            data.Columns[0].HeaderText = "X";
+            data.Columns[1].HeaderText = "Y";
+
+        }
+        //методы 
+
+        //добавление точки в ломаную линию 
+        public void addPoint(DataGridView data, int x, int y)
+        {
+            //добавляем координаты точки в массив 
+            points[N] = new Point(x, y);//добавляем строчку в таблицу данных datagridview
+            data.Rows.Add();
+            data.Rows[N].Cells[0].Value = x;
+            data.Rows[N].Cells[1].Value = y;
+            N++;
+        }
+
+
+        //метод для рисования ломаной линии
+        public void draw(PictureBox pbox)
+        {
+            //проверяем количество точек в ломаной 
+            if (N > 0)
+            {
+                //для picturebox создаем полтно на котором будем рисовать 
+                Graphics gr = pbox.CreateGraphics();
+                //созадем карандаш для рисования
+                Pen pen = new Pen(Color.Black, 2.1f);
+                //рисуем точки 
+                foreach(var a in points)
+                {
+                    gr.DrawEllipse(pen, a.X - 5, a.Y - 5, 10,10);
+                }
+                //рисуем отрезки для ломаной
+                for (int i = 1; i < N; i++)
+                {
+                    gr.DrawLine(pen, points[i - 1], points[i]);
+                }
+            }
+        }
+    }
+}
